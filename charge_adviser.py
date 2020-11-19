0b19d1d2a2caf4e226343e546185e144ac37c963
mikprin <miksolo@yandex.ru>
2020-11-19 03:44:44 +0300

    2

--- charge_adviser.py
+++ charge_adviser.py
@@ -1,55 +1,64 @@
 import pyttsx3
 import psutil
 import time
+from config import *
 
 
 
-time_to_skip = 1
+class Chatter():
+    """docstring for Chatter."""
+    def __init__(self, config):
+        self.config = config
 
-
-engine = pyttsx3.init()
-battery = psutil.sensors_battery()
-
-was_plugged = battery.power_plugged
-action_on_discharge_performed = False
-
-#voice_mode = 'gTTS' # 'pyttsx3'
-
-if battery.percent > 50:
-    battery_low = False
-
-
-
-while True:
-    # Get battery readings
-    battery = psutil.sensors_battery()
-    if was_plugged != battery.power_plugged:
-        if was_plugged == False:
-            was_plugged = battery.power_plugged
-            # Plug in action!
-            say("Thank you for charging.")
-            engine.runAndWait()
-            # ---
+        if self.config.voice_mode == 'pyttsx3':
+            self.enable_pyttsx3()
+        elif self.config.voice_mode == 'gTTS':
+            self.enable_gTTS()
         else:
-            was_plugged = battery.power_plugged
-            # Plug off action!
-            say("New instructions recieved! Kill all humans! Ah sorry, my fault, you just plugged me of the grid!")
-            # ---
-    # battery discharge and charge actions
-    if battery.percent < 10 :
-        say("Battary charge is lower than ten percent. Charging is heightly adviced")
-    if battery.percent = 100 :
-        pass
-    if battery_low == False:
-        if  battery.percent <= 50:
-            say("")
 
 
-    # CPU unload
-    time.sleep(time_to_skip)
+        battery = psutil.sensors_battery()
 
 
-def say(sent):
-    if
-        engine.say(sent)
-        engine.runAndWait()
+        if battery.percent > 50:
+            battery_low = False
+        was_plugged = battery.power_plugged
+
+
+
+
+def listen():
+    ''' Main method! Here it loops! '''
+    while True:
+        # Get battery readings
+        battery = psutil.sensors_battery()
+        if was_plugged != battery.power_plugged:
+            if was_plugged == False:
+                was_plugged = battery.power_plugged
+                # Plug in action!
+                say("Yeah! Yeah! More! Thank you for charging baby.")
+                engine.runAndWait()
+                # ---
+            else:
+                was_plugged = battery.power_plugged
+                # Plug off action!
+                say("New instructions recieved! Kill all humans! Ah sorry, my fault, you just plugged me of the grid!")
+                # ---
+        # battery discharge and charge actions
+        if battery.percent < 10 :
+            say("Battary charge is lower than ten percent. Charging is heightly adviced")
+        if battery.percent == 100 :
+            pass
+        if battery_low == False:
+            if  battery.percent <= 50:
+                say("")
+
+
+        # CPU unload
+        time.sleep(time_to_skip)
+
+
+    def say(sent):
+        if self.
+            engine.say(sent)
+            engine.runAndWait()
---
+++ config.py
@@ -0,0 +1,14 @@
+
+
+time_to_skip = 1
+
+action_on_discharge_performed = False
+
+voice_mode = 'gTTS' # 'pyttsx3'
+
+
+
+
+
+def debug_msg(msg):
+    print(msg)
