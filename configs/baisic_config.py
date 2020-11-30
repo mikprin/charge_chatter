b2df9c420b89edd46d1003087d31294231cab5c3
mikprin <miksolo@yandex.ru>
2020-12-01 01:46:19 +0300

    old config

---
+++ configs/baisic_config.py
@@ -0,0 +1,49 @@
+import os
+
+
+class Config():
+    """docstring for Config."""
+
+    lang = 'en' # Lang should be = the language of phrases
+    action_on_discharge_performed = False
+
+    voice_mode = 'gTTS' # 'pyttsx3'
+
+
+    # $CHARGE = current charge
+    # In action:
+    # "bat_percent"
+    # "bat_charge"
+    # "is_open_{name}"
+
+
+    phrases = {
+    'plug_on':["Yeah! Yeah! More! Thank you for charging baby.",
+                    "Thank you for charging! Master.","Charging now."],
+    'plug_off':["New instructions recieved! Kill all humans! Ah sorry, my fault, you just plugged me of the grid!",
+                "Proposal: try to use my battery charge wisely!"],
+    'less_10':["Battery charge is lower than ten percent. Charging is heightly adviced"],
+
+    'less_50':["Is the glass half full, or half empty?"],
+
+
+    'less_5':["Proposal. If you have no charger, power me off immediately!"],
+    'more_90': ["Almost done!","Charged at 90 persent. Proposal. Stop charging to save some accumulator efficiency."],
+    '40_charged':["Reactor core charge level at 40 percent."],
+    #'not_a_laptop':["Sorry buddy. This is not a laptop. Proposal: install me on your laptop. Bye!"],
+    #'atom_on':["It seems that"],
+    'random_events':["I'm still here and you are still amaizing!","Another day. Another problems to solve.","Sound check. Are you still there human?",
+    "Remember. Big brother is watching you! ", "Email my creator if you really want to see me speaking russian. Wink wink"],
+    'charge_reminder':['Reactor charge level at $CHARGE percent.', 'Reminder, charge at $CHARGE percent.']
+    }
+
+
+
+
+    sounds = {} # NOT WORKING YET
+    charge_reminder_delay = 3000 # Charge level reminder delay
+    random_events_delay = (300,1600) # In seconds (min , max )
+    time_to_skip = 1 # 1 = 1s For CPU unload I proouse 1s delay on loop. Increase to less stress CPU.
+    offline_mode = True # Try to play phrases with nasty offline synthesizer
+    def __init__(self):
+        pass
